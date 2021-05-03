bf0165f01244b419c1843e97276b709788aaa3ce
Rasul Dev <macosabdulqodir@MacBook-Air-MacosABdulqodir.local>
2021-05-03 16:28:19 +0500

    init

--- client/views.py
+++ client/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.utils.translation import gettext_lazy as _
+from .forms import RegistrationForm
+from django.views import View
 
-# Create your views here.
+
+class ClientRegistration(View):
+    def setup(self, request, *args, **kwargs):
+        super().setup(request, *args, **kwargs)
+
+        request.title = _('Ro\'yhatdan o\'tish')
+
+    def get(self, request):
+        return render(request, 'layouts/form.html', {
+            'form': RegistrationForm()
+        })
+
+    def post(self, request):
+        form = RegistrationForm(data=request.POST)
+
+        if form.is_valid():
+            user = form.user(commit=False)
+            user.set_password(user.password)
+            user.save()
+
+
+            messages.success(request, _("Siz muvaffaqiyatli ro'yhatdan o'tingiz !!!"))
+            return redirect("main:index")
+
+        return render(request, 'layouts/form.html', {
+            'form': form
+        })
--- gag/settings.py
+++ gag/settings.py
@@ -11,7 +11,12 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from django.contrib import messages
 
@@ -22,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4thp6+bn!ed_ipfe(k(b7^ne$$6t@numzaz%z(sg9=jmwb=5kw'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -40,7 +45,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
-    'client'
+    'client',
+    'crispy_forms',
+
 ]
 
 MIDDLEWARE = [
@@ -80,10 +87,7 @@ WSGI_APPLICATION = 'gag.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db()
 }
 
 
@@ -144,3 +148,7 @@ MESSAGE_TAGS = {
 STATICFILES_DIRS = [
     BASE_DIR / 'assets'
 ]
+
+AUTH_USER_MODEL = 'client.User'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
